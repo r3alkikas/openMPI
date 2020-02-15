f76c09327fb76a69b840742521f9581d01e84742
r3alkikas® <34173539+r3alkikas@users.noreply.github.com>
2020-02-15 15:45:02 +0000

    Final Progs

--- reduction.c
+++ reduction.c
@@ -2,22 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//compile: mpicc mpi_tst.c -o mpi
-//run: mpirun -np 3 -v mpi_tst
-
 int main(int argc, char *argv[]){
 
-    int rank, size,i sndcnt=1;
-    double reducao, mine;
-    char name[100];
+	int rank, size,i, sndcnt=1;
+	double reduction, mine;
+	char name[100];
 
-MPI_Init (&argc, &argv);
-mine = (rank + 1) * size;
-MPI_Reduce (&mine, &reducao, sndcnt, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
-MPI_Get_processor_name (name, &i);
-printf ("P:%d running at %s mine is %f\n", rank, name,mine);
-if(rank == 0)
-    printf("Root got this from reduction...%g\n", reducao);
-MPI_Finalize();
-return 0;
+	MPI_Init (&argc, &argv);
+	mine = (rank + 1) * size;
+	MPI_Reduce (&mine, &reduction, sndcnt, MPI_DOUBLE, MPI_MAX, 0, MPI_COMM_WORLD);
+	MPI_Get_processor_name (name, &i);
+	printf("P:%d running at %s mine is %f\n", rank,name,mine);
+	if (rank==0)
+		printf("Root got this from reduction...%g\n", reduction);
+	MPI_Finalize();
+	return 0;
 }
